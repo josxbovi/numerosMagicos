960cb8ac7cdf5e4c6215db33eeb8600a9c76e635
José Eugenio Navarro Bovi <jose.navarrobovi@alu.frt.utn.edu.ar>
2023-11-26 20:30:59 -0300

    Se realizo la pagina para el ejercicio 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('botonEmpezar').addEventListener('click', function() {
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById('contenedor').style.display = 'block';
+
+    document.getElementById('botonAdivinar').addEventListener('click', function() {
+      var respuestaUser = parseInt(document.getElementById('inputAdivinar').value);
+
+      if (respuestaUser === randomNumber) {
+        alert('¡Adivinaste el número!');
+      } else if (respuestaUser < randomNumber) {
+        alert('El número que ingresaste es menor al número mágico.');
+      } else {
+        alert('El número que ingresaste es mayor al número mágico.');
+      }
+    });
+  });
